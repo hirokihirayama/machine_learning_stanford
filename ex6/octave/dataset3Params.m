34eceb613adc0ad31630d87edbbda72205e38cd5
hirokihirayama <hiroki.hirayama1996@gmail.com>
2020-05-05 16:14:11 +0800

    Support Vector

--- ex6/octave/dataset3Params.m
+++ ex6/octave/dataset3Params.m
@@ -11,18 +11,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Fill in this function to return the optimal C and sigma
-%               learning parameters found using the cross validation set.
-%               You can use svmPredict to predict the labels on the cross
-%               validation set. For example,
-%                   predictions = svmPredict(model, Xval);
-%               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using
-%        mean(double(predictions ~= yval))
-%
-
+%Code Written
 C_test = [0.01 0.03 0.1 0.3 1 3 10 30]';
 sigma_test = [0.01 0.03 0.1 0.3 1 3 10 30]';
 lowest_error = inf ;
@@ -41,7 +30,7 @@ for i = 1:length(C_test);
     end
   end
 end
-
+%
 C=current_c
 sigma=current_sigma
 
